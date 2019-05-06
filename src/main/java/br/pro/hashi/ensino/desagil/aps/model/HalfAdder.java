45359f2a02fc7587a7993c789c532cc30dd81d0c
Pellizzon <38332600+Pellizzon@users.noreply.github.com>
2019-05-06 17:17:41 -0300

    envio

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
@@ -1,4 +1,60 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class HalfAdder extends Gate {
+    private final NandGate nandLeft;
+    private final NandGate nandRight;
+    private final NandGate nandTop;
+    private final NandGate nandBottom;
+    private final NandGate nandBottomB;
+
+
+    public HalfAdder() {
+        super("Half-Adder", 2, 2);
+
+        nandLeft = new NandGate();
+        nandTop = new NandGate();
+        nandBottom = new NandGate();
+        nandBottomB = new NandGate();
+        nandRight = new NandGate();
+
+        nandTop.connect(1, nandLeft);
+        nandBottom.connect(0, nandLeft);
+
+        nandRight.connect(0, nandTop);
+        nandRight.connect(1, nandBottom);
+
+        nandBottomB.connect(0,nandLeft);
+        nandBottomB.connect(1, nandLeft);
+    }
+
+
+    @Override
+    public boolean read(int outputPin) {
+        if (outputPin != 0 && outputPin != 1) {
+            throw new IndexOutOfBoundsException(outputPin);
+        }
+
+        if (outputPin == 0) {
+            return nandRight.read();
+        } else {
+            return nandBottomB.read();
+        }
+    }
+
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        switch (inputPin) {
+            case 0:
+                nandTop.connect(0, emitter);
+                nandLeft.connect(0, emitter);
+                break;
+            case 1:
+                nandLeft.connect(1, emitter);
+                nandBottom.connect(1, emitter);
+                break;
+            default:
+                throw new IndexOutOfBoundsException(inputPin);
+        }
+    }
 }
--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -20,7 +20,7 @@ public class GateView extends FixedPanel implements ItemListener {
     private final Switch[] switches;
     private final Gate gate;
     private final JCheckBox[] inputBoxes;
-    private final JCheckBox outputBox;
+    private final JCheckBox[] outputBoxes;
     private final Image image;
 
     public GateView(Gate gate) {
@@ -29,9 +29,11 @@ public class GateView extends FixedPanel implements ItemListener {
         this.gate = gate;
 
         int inputSize = gate.getInputSize();
+        int outputSize = gate.getOutputSize();
 
         switches = new Switch[inputSize];
         inputBoxes = new JCheckBox[inputSize];
+        outputBoxes = new JCheckBox[outputSize];
 
         for (int i = 0; i < inputSize; i++) {
             switches[i] = new Switch();
@@ -40,9 +42,12 @@ public class GateView extends FixedPanel implements ItemListener {
             gate.connect(i, switches[i]);
         }
 
-        outputBox = new JCheckBox();
+        for (int i=0; i< outputSize; i++) {
+            outputBoxes[i] = new JCheckBox();
+        }
 
-        int x, y, step;
+
+        int x, y, step, stepout;
 
         x = BORDER;
         y = -(SWITCH_SIZE / 2);
@@ -52,7 +57,15 @@ public class GateView extends FixedPanel implements ItemListener {
             add(inputBox, x, y, SWITCH_SIZE, SWITCH_SIZE);
         }
 
-        add(outputBox, BORDER + SWITCH_SIZE + GATE_WIDTH, (GATE_HEIGHT - SWITCH_SIZE) / 2, SWITCH_SIZE, SWITCH_SIZE);
+        int yout = -(SWITCH_SIZE / 2);
+        int xout = BORDER + SWITCH_SIZE + GATE_WIDTH;
+        stepout = (GATE_HEIGHT / (outputSize + 1));
+        for (JCheckBox outputBox : outputBoxes) {
+            yout += stepout;
+            add(outputBox, xout, yout, SWITCH_SIZE, SWITCH_SIZE);
+
+            outputBox.setEnabled(false);
+        }
 
         String name = gate.toString() + ".png";
         URL url = getClass().getClassLoader().getResource(name);
@@ -61,8 +74,6 @@ public class GateView extends FixedPanel implements ItemListener {
         for (JCheckBox inputBox : inputBoxes) {
             inputBox.addItemListener(this);
         }
-
-        outputBox.setEnabled(false);
 
         update();
     }
@@ -76,9 +87,12 @@ public class GateView extends FixedPanel implements ItemListener {
             }
         }
 
-        boolean result = gate.read();
-
-        outputBox.setSelected(result);
+        for (int i=0; i<gate.getOutputSize(); i++) {
+            for (JCheckBox outbox : outputBoxes) {
+                boolean result = gate.read(i);
+                outbox.setSelected(result);
+            }
+        }
     }
 
     @Override
